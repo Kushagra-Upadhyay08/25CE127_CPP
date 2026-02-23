753b272ef74f5cf1796c3cb21ce618179906dfa2
Kushagra VU <ukushagra8@gmail.com>
2026-02-23 10:59:03 +0530

    Refactor loan application to include default loan
    Updated loan application system to include default loan display and user input for loan details.

--- Practical 2.5/main.cpp
+++ Practical 2.5/main.cpp
@@ -1,72 +1,36 @@
 #include "header.h"
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-int main(){
-    // Generateing an switch case based system for the user to interact with the program
-    int t=0,i,choice;
-    double id;
+int main() {
+    int id, tenure;
     string name;
-    double amount;
-    float intrest;
-    int tenure;
-    loan l[100];
+    double amount, rate;
 
-    while(1){
-        cout<<"1.Add a new loan application"<<endl;
-        cout<<"2.Display all loan applications"<<endl;
-        cout<<"3.Calculate EMI for a loan application"<<endl;
-        cout<<"4.Exit"<<endl;
-        cout<<"Enter your choice: ";
-        cin>>choice;
-        switch(choice){
-            case 1:
-                cout<<"Enter Loan ID: ";
-                cin>>id;
-                cout<<"Enter Applicant Name: ";
-                cin>>name;
-                cout<<"Enter Loan Amount: ";
-                cin>>amount;
-                cout<<"Enter Annual Intrest: ";
-                cin>>intrest;
-                cout<<"Enter Loan Tenure: ";
-                cin>>tenure;
-                l[t++]=loan(id,name,amount,intrest,tenure);
-                break;
-                
-            case 2:
-            if(t==0){
-                cout<<"Not a single Loan is not applied yet"<<endl;
-                break;
-            }
-                for(i=0;i<t;i++){
-                    l[i].display();
-                }
-                break;
-            
-            case 3:
-                cout<<"Enter Loan ID: ";
-                cin>>id;
-                for(i=0;i<t;i++){
-                    if(l[i].search(id)){
-                        
-                        cout<<"EMI: "<<l[i].calc_emi()<<endl;    
-                    }
-                }
-                if(i==t){
-                    cout<<"Loan ID not found"<<endl;
-                }
-                break;
-            case 4:
-                
-                exit(0);
-            default:
-                cout<<"Invalid choice"<<endl;
-            
-    }
-    
-}
+    Loan defaultLoan;
+    cout << "\nDisplaying Default Loan:";
+    defaultLoan.displayDetails();
 
-return 0;
+    cout << "\n\nEnter Loan ID: ";
+    cin >> id;
+
+    cout << "Enter Applicant Name: ";
+    cin.ignore();
+    getline(cin, name);
+
+    cout << "Enter Loan Amount: ";
+    cin >> amount;
+
+    cout << "Enter Annual Interest Rate (%): ";
+    cin >> rate;
+
+    cout << "Enter Loan Tenure (in months): ";
+    cin >> tenure;
+
+    Loan userLoan(id, name, amount, rate, tenure);
+
+    cout << "\nDisplaying User Loan:";
+    userLoan.displayDetails();
+
+    return 0;
 }
